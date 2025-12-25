3596f8d0333155d5099fe3a5f8891a73ecefee84
Insu <71576936+Kilisaki@users.noreply.github.com>
2025-12-25 20:54:36 +0500

    Fixed: CRUD & Accesibility of Profiles page

--- routes/web.php
+++ routes/web.php
@@ -1,44 +1,48 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
 
 Route::get('/home', function () {
     return view('home');
-})->name('home');
+})->name('home.alt');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 // Public product routes
-Route::get('/products', [App\Http\Controllers\ProductController::class, 'index'])->name('products.index');
-Route::get('/products/{product}', [App\Http\Controllers\ProductController::class, 'show'])->name('products.show');
+Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+Route::get('/products/{product}', [ProductController::class, 'show'])
+    ->name('products.show')
+    ->where('product', '[0-9]+'); // Important: numeric constraint
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    // Protected product routes (create/update/delete)
-    Route::resource('products', App\Http\Controllers\ProductController::class)->except(['index', 'show']);
+    // Protected product management routes
+    Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
+    Route::post('/products', [ProductController::class, 'store'])->name('products.store');
+    Route::get('/products/{product}/edit', [ProductController::class, 'edit'])->name('products.edit');
+    Route::patch('/products/{product}', [ProductController::class, 'update'])->name('products.update');
+    Route::delete('/products/{product}', [ProductController::class, 'destroy'])->name('products.destroy');
 
-    Route::get('products/trashed', [App\Http\Controllers\ProductController::class, 'trashed'])
-        ->name('products.trashed');
-    Route::patch('products/{id}/restore', [App\Http\Controllers\ProductController::class, 'restore'])
-        ->name('products.restore');
-    Route::delete('products/{id}/force-delete', [App\Http\Controllers\ProductController::class, 'forceDelete'])
-        ->name('products.force-delete');
+    // Soft delete management
+    Route::get('products/trashed', [ProductController::class, 'trashed'])->name('products.trashed');
+    Route::patch('products/{id}/restore', [ProductController::class, 'restore'])->name('products.restore');
+    Route::delete('products/{id}/force-delete', [ProductController::class, 'forceDelete'])->name('products.force-delete');
 
-    // Users listing and user products (by username)
-    Route::get('users', [App\Http\Controllers\UserController::class, 'index'])
-        ->name('users.index');
-    Route::get('users/{username}/objects', [App\Http\Controllers\UserController::class, 'objects'])
-        ->name('users.objects');
+    // User management
+    Route::get('users', [UserController::class, 'index'])->name('users.index');
+    Route::get('users/{username}/objects', [UserController::class, 'objects'])->name('users.objects');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__.'/auth.php';        
